85a137e1e106ce96bed48a93a2ed4b9ffb3dd33a
sivagamirajaselvam <134798295+sivagamirajaselvam@users.noreply.github.com>
2023-07-19 23:23:06 +0530

    Add files via upload

---
+++ summary/summary.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css']
+})
+export class SummaryComponent implements OnInit {
+  constructor(private rou:ActivatedRoute,private router:Router){}
+
+  ngOnInit(): void {
+    
+  }
+  
+  
+   
+      }
+  
+
